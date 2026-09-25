4a16a28ff28a1e13967ce41a6b9cd40cf78920f7
ym9079389-alt <ym9079389@gmail.com>
2026-09-25 14:41:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkShop.Net._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
